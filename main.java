2dc9a3761cf9880c16adb883971ccad41c8e85bf
Hanyi Ye <129237207+HanyiYe1@users.noreply.github.com>
2023-05-05 17:49:41 -0400

    Add files via upload

---
+++ main.java
@@ -0,0 +1,127 @@
+import java.util.Scanner;
+import java.util.HashMap;
+
+public class main {
+
+	//
+	// Functions
+	//
+	
+	static void printMenu() {
+		System.out.println("Customer and Sales System \n "
+				+ "1. Enter Customer Information \n "
+				+ "2. Generate Customer data file \n "
+				+ "3. Report on total Sales \n "
+				+ "4. Check for fraud in sales data \n "
+				+ "9. Quit\n "
+				+ "Enter menu option (1-9)");
+	}
+	
+	static void enterCustomerInfo() {
+		
+	}
+	
+	static void validatePostalCode() {
+		
+	}
+	
+	static void validateCreditCard() {
+		
+	}
+	
+	static void generateCustomerDataFile() {
+		
+	}
+	
+	static void reportSalesData() {
+		
+	}
+	
+	static void calculatePercentage() {
+		
+	}
+	
+	static void generateGraph() {
+		
+	}
+	
+	static void fraudCheck() {
+		
+	}
+	
+	static void generateReport() {
+		
+	}
+	
+	//####################################################################
+	//#       ADDITIONAL METHODS MAY BE ADDED BELOW IF NECESSARY         #
+	//####################################################################
+
+	static void initiateHashMaps(HashMap<String, Integer> amount) {
+		amount.put("1", 0);
+		amount.put("2", 0);
+		amount.put("3", 0);
+		amount.put("4", 0);
+		amount.put("5", 0);
+		amount.put("6", 0);
+		amount.put("7", 0);
+		amount.put("8", 0);
+		amount.put("9", 0);
+	}
+	
+	
+	
+	//####################################################################
+	//#                            MAIN PROGRAM                          #
+	//#           DO NOT EDIT ANY CODE EXCEPT WHERE INDICATED            #
+	//####################################################################
+	
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		Scanner scanner = new Scanner(System.in);
+		HashMap<String, Integer> firstDigitAmount = new HashMap<String, Integer>();
+		HashMap<String, Integer> firstDigitPercentage = new HashMap<String, Integer>();
+		
+		String userInput = "";
+		String enterCustomerOption = "1";
+		String generateCustomerOption = "2";
+		String reportSalesData = "3";
+		String checkForFraud = "4";
+		String exitCondition = "9";
+		
+		//Set up the 'dictionaries'
+		initiateHashMaps(firstDigitAmount);
+		initiateHashMaps(firstDigitPercentage);
+		
+		while (!userInput.equals(exitCondition)) {
+			printMenu();
+			userInput = scanner.nextLine();
+
+			if (userInput.equals(enterCustomerOption)) {
+				enterCustomerInfo();
+			}
+			
+			else if (userInput.equals(generateCustomerOption)) {
+				generateCustomerDataFile();
+			}
+			
+			else if (userInput.equals(reportSalesData)) {
+				reportSalesData();
+			}
+			
+			else if (userInput.equals(checkForFraud)) {
+				fraudCheck();
+			}
+			
+			else if (userInput.equals(exitCondition)) {
+				scanner.close();
+				break;
+			}
+			
+			else {
+				System.out.println("Please type in a valid option (A number from 1-9)");
+			}
+		}
+		System.out.println("Program Terminated");
+	}
+}
