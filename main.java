55cf7a17e62be554d544b36c9e6821e2b89487cd
BensonLiu06 <129234480+BensonLiu06@users.noreply.github.com>
2023-05-11 23:27:33 -0400

    Added Credit Card validation function

--- main.java
+++ main.java
@@ -1,8 +1,12 @@
 import java.util.Scanner;
+import java.io.BufferedReader;
+
 import java.io.File;
 import java.util.HashMap;
 import java.util.ArrayList;
 import java.io.FileNotFoundException;
+import java.io.FileReader;
+import java.io.IOException;
 
 
 public class main {
@@ -10,6 +14,10 @@ public class main {
 	// Functions
 	//
 	
+	private static String creditCard;
+
+
+
 	static void printMenu() {
 		System.out.println("Customer and Sales System \n "
 					+ "1. Enter Customer Information \n "
@@ -20,87 +28,92 @@ public class main {
 					+ "Enter menu option (1-9)");
 	}
 	
-	static void enterCustomerInfo(ArrayList<ArrayList<String>> customerInfomation) {
+	static void enterCustomerInfo(ArrayList<ArrayList<String>> customerInfomation) throws FileNotFoundException {
 		ArrayList<String> storage = new ArrayList<String>();
 		Scanner scanner = new Scanner(System.in);
-
-		String postalCode;
-		String creditCard;
 		System.out.println("Enter your first Name: ");
 		String firstName = scanner.nextLine();
 
 		System.out.println("Enter your last name: ");
 		String lastName = scanner.nextLine();
 
-		System.out.println(" Enter the city you are from: ");
+		System.out.println("Enter the city you are from: ");
 		String city = scanner.nextLine();
 
 			while (true) {
 				System.out.println("Enter Postal Code: ");
-				postalCode = scanner.nextLine();
+				String postalCode = scanner.nextLine();
 				
 				if (postalCode.length() > 3 && main.validatePostalCode(postalCode, postalCode)) {
 					break;
 				}
-				else if (postalCode.length() < 3) {
+				else if (postalCode.length() < 3 || postalCode.length() > 6) {
 					System.out.println("Invalid amount of digits");
 				}
 
 			while (true) {
 				System.out.println("Enter Credit Card Number: ");
-				creditCard = scanner.nextLine(); 
+				String creditCard = scanner.nextLine(); 
 
-				String space = creditCard.replaceAll("\\s+ " , "");
 
-				if (creditCard.length() == 12 && validateCreditCard(space)) {
+				if (creditCard.length() == 16 && main.validateCreditCard(creditCard)) {
 					break;
 				}
-				else if (creditCard.length() < 12 || creditCard.length() > 12 ) {
+				else if (creditCard.length() < 16 || creditCard.length() > 16 ) {
 					System.out.println("Please enter the correct amount of digits for your Credit Card");
 				
 				}
 				
 			}
-			storage.add(firstName);
-			storage.add(lastName);
-			storage.add(city);
-			storage.add(postalCode);
-			storage.add(creditCard);
+	
+			
+		storage.add(firstName);
+		storage.add(lastName);
+		storage.add(city);
+		storage.add(postalCode);
+		storage.add(creditCard);
 
-			customerInfomation.add(storage);
+		customerInfomation.add(storage);
 		}
-
+		scanner.close();
 	}
 	
-	static boolean validatePostalCode(String enterCustomerInfo , String postalCode) {
+	static boolean validatePostalCode(String enterCustomerInfo, String postalCode) throws FileNotFoundException {
 		
-		File postalFile = new File("postal_codes.csv");
-		Scanner readFile = new Scanner(postalFile);
+		try (BufferedReader br = new BufferedReader(new FileReader("postal_codes.csv"))) {
+            String line;
+            while ((line = br.readLine()) != null) {
+                if (line.startsWith(postalCode.substring(0, 3))) {
+                    System.out.println("Postal Code Validated");
+                    return true;
+                }
+            }
+            return false;
+        } catch (IOException e) {
+            System.err.format("IOException: %s%n", e);
+            System.out.println("Enter a valid postal code");
+            return false;
+        }
+    }
 
-		
-		String firstCharacters = readFile.nextLine().substring(0,3);
-		String first_Postal_Code = postalCode.substring(0,3);
-
-		if (first_Postal_Code == firstCharacters) {
-			System.out.println("Postal Code Validated");
-			return false;
-
-		}
-		else {
-			System.out.println(" Invalid Postal Code ");
-			return true;
-
-		
-		}
-
-		
-
-
-
-	}
 
 	static boolean validateCreditCard( String creditCard) {		
-		return false;
+		int sum = 0;
+		boolean alternate = false;
+		for (int i = creditCard.length() - 1; i >= 0; i--) {
+		int n = Integer.parseInt(creditCard.substring(i, i + 1));
+		if (alternate) {
+			n *= 2;
+			if (n > 9) {
+			n = (n % 10) + 1;
+			}
+		}
+		sum += n;
+		alternate = !alternate;
+		}
+		return (sum % 10 == 0);
+	
+		
 
 	}
 	
@@ -151,14 +164,13 @@ public class main {
 	//#           DO NOT EDIT ANY CODE EXCEPT WHERE INDICATED            #
 	//####################################################################
 	
-	public static void main(String[] args) {
+	public static void main(String[] args) throws FileNotFoundException {
 		// TODO Auto-generated method stub
 		Scanner scanner = new Scanner(System.in);
 		HashMap<String, Integer> firstDigitAmount = new HashMap<String, Integer>();
 		HashMap<String, Integer> firstDigitPercentage = new HashMap<String, Integer>();
-		ArrayList<ArrayList<String>> customerInformation = new ArrayList<ArrayList<String>>();
+		ArrayList<ArrayList<String>> customerInfo= new ArrayList<ArrayList<String>>();
 
-		String[] customerInfo = new String[5];
 		
 		String userInput = "";
 		String enterCustomerOption = "1";
@@ -176,6 +188,7 @@ public class main {
 			userInput = scanner.nextLine();
 
 			if (userInput.equals(enterCustomerOption)) {
+				
 				enterCustomerInfo(customerInfo);
 			}
 			
